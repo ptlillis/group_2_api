743449d47eff75fec59bd69eca1b09c693b71103
Arjun Ghimire <ghimirearjun15@gmail.com>
2021-01-05 21:54:22 -0500

    gify and youtube changes with embed link

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var minuteLived = document.querySelector('.heading5');
 var zodiacv = document.querySelector("#zodiac");
+var youtubediv = document.querySelector(".youtube-div");
+var gifydiv = document.querySelector(".gify-div");
+var trackinfo = document.querySelector(".track");
+var artist = document.querySelector(".artist")
 console.log("app is connected")
 $(document).ready(function () {
     // setting up the max date input to limit the input value
@@ -17,6 +21,7 @@ $(document).ready(function () {
         if (inputdate == "Invalid Date") {
             return;
         }
+        apicall();
         var inputyear = inputdate.getFullYear();
         console.log(inputyear);
         
@@ -148,141 +153,82 @@ $(document).ready(function () {
          zodiacv.appendChild(image);
    }
   });
-// $(document).ready(function () {
-//     var myHeaders = new Headers();
-//     myHeaders.append("x-rapidapi-key", "936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891");
-//     var requestOptions = {
-//         method: 'GET',
-//         headers: myHeaders,
-//         redirect: 'follow'
-//     };
-//     fetch("https://billboard-api2.p.rapidapi.com/hot-100?date=2019-05-11&range=1-10&x-rapidapi-key=936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891", requestOptions)
-//         .then(response => response.json())
-//         .then(result => {
-//             console.log(result);
-//             var title = result['content']['1']['title'];
-//             console.log(title);
-//             var artists = JSON.stringify(result['content']['1']['artist']);
-//             console.log(artists);
-//             var artist = artists['0'];
-//             console.log(artist);
-//             var weeks = result['content']['1']['weeks at no.1'];
-//             console.log("this song stay no.1 for " + weeks + ' weeks');
-//             youtubesearch()
-//             function youtubesearch() {
-//                 var myHeaders = new Headers();
-//                 myHeaders.append("x-rapidapi-key", " 936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891");
-//                 var requestOptions = {
-//                     method: 'GET',
-//                     headers: myHeaders,
-//                     redirect: 'follow'
-//                 };
-//                 fetch("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + title + artists, requestOptions)
-//                     .then(response => response.json())
-//                     .then(result => {
-//                         var link = result['items']['1']['link'];
-//                         console.log(result)
-//                     })
-//                     .catch(error => console.log('error', error))
-//             }
-//             gifysearch()
-//             function gifysearch() {
-//                 var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + title + "&api_key=dc6zaTOxFJmzC&limit=10";
-//                 $.ajax({
-//                     method: "GET",
-//                     url: queryURL,
-//                 })
-//                     .then(function (response) {
-//                         for (var i = 0; i < 3; i++) {
-//                             var embedUrl = response['data'][i]['embed_url'];
-//                         }
-//                         console.log(response);
-//                     })
-//             }
-//         })
-//         .catch(error => console.log('error', error));
-// });
 
-// //this only when page load we need to run this by click event.
-// $(document).ready(function () {
-//     var myHeaders = new Headers();
-//     myHeaders.append("x-rapidapi-key", "936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891");
-//     var requestOptions = {
-//         method: 'GET',
-//         headers: myHeaders,
-//         redirect: 'follow'
-//     };
-//     fetch("https://billboard-api2.p.rapidapi.com/hot-100?date=2019-05-11&range=1-10&x-rapidapi-key=936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891", requestOptions)
-//         .then(response => response.json())
-//         .then(result => {
-//             //console logging data and grabbing th artist and songs title form data and passing that data to other function.
-//             console.log(result);
-//             var title = result['content']['1']['title'];
-//             console.log(title);
-//             var artists = JSON.stringify(result['content']['1']['artist']);
-//             console.log(artists);
-//             //var artist = artists['0'];
-//             //comented out because I was trying to get out only one artist but it shows the first letter 
-//             console.log(artist);
-//             var weeks = result['content']['1']['weeks at no.1'];
-//             console.log("this song stay no.1 for " + weeks + ' weeks');
-//             //you can append this weeks and artist as a artist and song information
-//             youtubesearch()
-//             function youtubesearch() {
-//                 var myHeaders = new Headers();
-//                 myHeaders.append("x-rapidapi-key", " 936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891");
-//                 var requestOptions = {
-//                     method: 'GET',
-//                     headers: myHeaders,
-//                     redirect: 'follow'
-//                 };
-//                 fetch("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + title + artists, requestOptions)
-//                     .then(response => response.json())
-//                     .then(result => {
-//                         var link = result['items']['1']['link'];
-//                         //document.getElementById('header').innerHTML = "<iframe title='YouTube video player'   type=\'text/html\' width='640' height='390' src=" + link + "frameborder='0' allowFullScreen></iframe>"
-//                         //just change the id and you willbe done and change width and height according to the size of appending div.
-//                         // and uncomment (comented out to run the function without any error)
-//                         // you need to put that link to dom element by creating source that can handle url and play.
-//                         console.log(result)
-//                     })
-//                     .catch(error => console.log('error', error))
-//             }
-//             gifysearch()
-//             function gifysearch() {
-//                 var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + title + "&api_key=dc6zaTOxFJmzC&limit=10";
-//                 $.ajax({
-//                     method: "GET",
-//                     url: queryURL,
-//                 })
-//                     //forever loop to grab 4 gify images and append to the html
-//                     .then(function (response) {
-//                         for (var i = 0; i < 4; i++) {
-//                             var fig = document.createElement('figure');
-//                             var img = document.createElement('img');
-//                             var fc = document.createElement('figcaption');
-//                             var embedUrl = response['data'][i]['embed_url'];
-//                             img.src = embedUrl;
-//                             img.alt = response['data'][i]['title'];
-//                             fc.textContent = response['data'][i]['title'];
-//                             fig.appendChild(img);
-//                             fig.appendChild(fc);
-//                             var div = document.createElement('div');
-//                             div.append(fig);
-//                             //you need to append that div to the html element.
-//                         }
-//                         console.log(response);
-//                     })
-//             }
-//         })
-//         .catch(error => console.log('error', error));
-// });
-// //this is the expression for minutes you lived but need to grab that input value year month and day and subtract respectively with the curreent year month and day
-// //and parsefloat that value to chage to number and pass that value as year month and day
-// //   var year = parseFloat(yearvalue);
-// //   var month = parseFloat(monthvalue);
-// //   var day = parseFloat(dayvalue);
-// //   var monthsYouLived =(yer*12)+month;
-// //   var dayYouLived = (monthsYouLived * 30)+ day;
-// //   var minutesYouLived = (dayYouLived) * 24 * 60;
-// // comented out for now
+function apicall() {
+  var inputvalue = $("#input").val(); 
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", "a23b713ff9mshc20756353bc1f49p17a981jsn336d01178ed0");
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    fetch("https://billboard-api2.p.rapidapi.com/hot-100?date="+inputvalue+"&range=1-10&x-rapidapi-key=a23b713ff9mshc20756353bc1f49p17a981jsn336d01178ed0", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            //console logging data and grabbing th artist and songs title form data and passing that data to other function.
+            console.log(result);
+           
+            var title = result['content']['1']['title'];
+            console.log(title);
+            var h3 = document.createElement('h3');
+            h3.textContent= title;
+            trackinfo.append(h3);
+            var artists = result['content']['1']['artist'];
+            console.log(artists);
+            artist.textContent = artists;
+            var weeks = result['content']['1']['weeks at no.1'];
+            console.log("this song stay no.1 for " + weeks + ' weeks');
+            trackinfo.textContent = "this song stay no.1 for "+ weeks + "weeks";
+            //you can append this weeks and artist as a artist and song information
+            youtubesearch();
+            function youtubesearch() {
+                var myHeaders = new Headers();
+                myHeaders.append("x-rapidapi-key", " 936158cc87mshd1a476eec07db95p17f6cbjsnea52f1179891");
+                var requestOptions = {
+                    method: 'GET',
+                    headers: myHeaders,
+                    redirect: 'follow'
+                };
+                fetch("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + title + artists, requestOptions)
+                    .then(response => response.json())
+                    .then(result => {
+                        var embedlink = result['items']['1']['link'];
+                        var iframe = document.createElement("iframe");
+                        iframe.setAttribute("style","height=100%");
+                        iframe.setAttribute("style","width=100%");
+                        console.log(result);
+                          var res = embedlink.split("=");
+                          var embeddedUrl = "https://www.youtube.com/embed/"+res[1];
+                          iframe.src = embeddedUrl;
+                          youtubediv.append(iframe);  
+                    })
+                    .catch(error => console.log('error', error));
+                   
+            }
+            gifysearch();
+            function gifysearch() {
+                var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + artists + "&api_key=dc6zaTOxFJmzC&limit=4";
+                $.ajax({
+                    method: "GET",
+                    url: queryURL,
+                })
+                    //forever loop to grab 4 gify images and append to the html
+                    .then(function (response) {
+                        for (var i = 0; i < 4; i++) {
+                            var iframe = document.createElement('iframe');
+                           
+                           var embedUrl = response['data'][i]['embed_url'];
+                            iframe.src = embedUrl;
+                            iframe.setAttribute('style',"height=350px")
+                            iframe.setAttribute('style',"width=350px")
+                            var div = document.createElement('div');
+                            div.append(iframe);
+                            gifydiv.appendChild(div);
+                        }
+                        console.log(response);
+                    })
+            }
+        })
+        .catch(error => console.log('error', error));
+};
